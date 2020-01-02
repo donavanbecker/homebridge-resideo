9573da72858a77c11cc2aef6a54fd0e68f0556b1
oznu <dev@oz.nu>
2020-01-02 18:12:21 +1100

    add zero-config portal ui for homebridge-config-ui-x

---
+++ server/portal/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  api: {
+    base: '',
+  },
+  honeywell: {
+    redirectUrl: 'https://homebridge-honeywell.iot.oz.nu/link-account',
+    consumerKey: 'RTrCH5utygGV3vBymbbDdRI2yNh9aUj0'
+  }
+};
