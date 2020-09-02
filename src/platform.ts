94245eccd79577f0ba98ed678b6c49f8843f1100
Donavan Becker <donavan.becker@icloud.com>
2020-09-02 12:28:05 -0500

    Update platform.ts

--- src/platform.ts
+++ src/platform.ts
@@ -132,6 +132,9 @@ export class HoneywellHomePlatform implements DynamicPlatformPlugin {
 
     // Room Priority Config Options
     this.config.options.roompriority.kind;
+    if (this.config.options.roompriority.kind === 'true') {
+      this.config.options.roompriority.kind = this.config.options.roompriority.hide;
+    }
 
     this.config.options.ttl = this.config.options.ttl || 1800; // default 1800 seconds
 
@@ -429,7 +432,7 @@ export class HoneywellHomePlatform implements DynamicPlatformPlugin {
                       // create the accessory handler for the restored accessory
                       // this is imported from `platformAccessory.ts`
                       new RoomSensorThermostat(this, existingAccessory, locationId, device, findaccessories, group, this.rooms);
-                    } else if (this.config.options.roompriority.kind === 'hide' || this.config.options.roompriority.kind !== 'thermostat' || this.config.options.roompriority.kind === 'switches' || this.config.options.thermostat.hide || this.config.options.roomsensor.hide || !device.isAlive) {
+                    } else if (this.config.options.roompriority.hide || this.config.options.roompriority.kind !== 'thermostat' || this.config.options.roompriority.kind === 'switches' || this.config.options.thermostat.hide || this.config.options.roomsensor.hide || !device.isAlive) {
                       // remove platform accessories when no longer present
                       this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
                       this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
