cbaa6ec1c10db68b89b042ef045e0de1f20963a9
Donavan Becker <donavan.becker@icloud.com>
2021-08-01 06:37:37 -0500

    create local server for auth

--- homebridge-ui/server.js
+++ homebridge-ui/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* jshint node: true,esversion: 9, -W014, -W033 */
@@ -5,7 +6,10 @@
 'use strict';
 
 const { HomebridgePluginUiServer } = require('@homebridge/plugin-ui-utils');
+const exec = require('child_process').exec;
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
 class PluginUiServer extends HomebridgePluginUiServer {
   constructor() {
@@ -15,6 +19,100 @@ class PluginUiServer extends HomebridgePluginUiServer {
       A native method getCachedAccessories() was introduced in config-ui-x v4.37.0
       The following is for users who have a lower version of config-ui-x
     */
+
+    this.onRequest('/startServer', () => {
+      const runningServer = http.createServer(function (req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        const urlParts = url.parse(req.url, true);
+        const pathArr = urlParts.pathname.split('?');
+        const action = pathArr[0].replace('/', '');
+        const query = urlParts.query;
+        switch (action) {
+          case 'start': {
+            this.key = urlParts[2];
+            this.secret = urlParts[3];
+            const url = 'https://api.honeywell.com/oauth2/authorize?' +
+              'response_type=code&redirect_uri=' + encodeURI('http://127.0.0.1:64911/auth') + '&' +
+              'client_id=' + query.key;
+            res.end('<script>window.location.replace(\'' + url + '\');</script>');
+            break;
+          }
+          case 'auth': {
+            if (query.code) {
+              const code = query.code;
+              const auth = Buffer.from(this.key + ':' + this.secret).toString('base64');
+              let curlString = '';
+              curlString += 'curl -X POST ';
+              curlString += '--header "Authorization: Basic ' + auth + '" ';
+              curlString += '--header "Accept: application/json" ';
+              curlString += '--header "Content-Type: application/x-www-form-urlencoded" ';
+              curlString += '-d "';
+              curlString += 'grant_type=authorization_code&';
+              curlString += 'code=' + code + '&';
+              curlString += 'redirect_uri=' + encodeURI('http://127.0.0.1:64911/auth');
+              curlString += '" ';
+              curlString += '"https://api.honeywell.com/oauth2/token"';
+
+              console.log(curlString);
+              console.log(query);
+              // eslint-disable-next-line @typescript-eslint/no-unused-vars
+              exec(curlString, (error, stdout, stderr) => {
+                const response = JSON.parse(stdout);
+                console.log(JSON.stringify(response));
+                if (response.access_token) {
+                  this.pushEvent('creds-received', {
+                    access: response.access_token,
+                    refresh: response.refresh_token,
+                  });
+                } else {
+                  this.pushEvent('creds-received', {
+                    error: JSON.stringify(response),
+                  });
+                }
+
+                if (error !== null) {
+                  console.log('exec error: ' + JSON.stringify(error));
+                }
+                if (stderr !== null) {
+                  console.log('exec stderr: ' + JSON.stringify(stderr));
+                }
+
+                res.end(JSON.stringify(response));
+                if (error) {
+                  res.end('error - ' + JSON.stringify(error));
+                }
+                console.log(JSON.stringify(res));
+                console.log(JSON.stringify(response));
+              });
+              res.end('not sure how we get here');
+            } else {
+              res.end('an error occurred. close this window and try again.');
+            }
+            break;
+          }
+          default: {
+            // should never happen
+            res.end('welcome to the server');
+            break;
+          }
+        }
+      });
+      runningServer.listen(64911, err => {
+        if (err) {
+          Console.log(err);
+        }
+      });
+
+      setTimeout(() => {
+        runningServer.close();
+      }, 300000);
+    });
+
+    /*
+      A native method getCachedAccessories() was introduced in config-ui-x v4.37.0
+      The following is for users who have a lower version of config-ui-x
+    */
+
     this.onRequest('/getCachedAccessories', async () => {
       try {
         // Define the plugin and create the array to return
