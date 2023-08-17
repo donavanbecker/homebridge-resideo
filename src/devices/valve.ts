6ea6855cfb2d632542899846462276b8e93767f6
Donavan Becker <beckersmarthome@icloud.com>
2023-08-16 19:44:23 -0500

    Update valve.ts

--- src/devices/valve.ts
+++ src/devices/valve.ts
@@ -13,18 +13,9 @@ import * as settings from '../settings';
 export class Valve {
   // Services
   service: Service;
-  temperatureService?: Service;
-  humidityService?: Service;
-  leakService?: Service;
 
   // CharacteristicValue
-  StatusActive!: CharacteristicValue;
-  LeakDetected!: CharacteristicValue;
-  BatteryLevel!: CharacteristicValue;
-  ChargingState!: CharacteristicValue;
-  StatusLowBattery!: CharacteristicValue;
-  CurrentTemperature!: CharacteristicValue;
-  CurrentRelativeHumidity!: CharacteristicValue;
+  Active!: CharacteristicValue;
 
   // Others
   action!: string;
@@ -62,8 +53,8 @@ export class Valve {
 
     // get the LightBulb service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
-    (this.service = this.accessory.getService(this.platform.Service.Battery) || this.accessory.addService(this.platform.Service.Battery)),
-    `${accessory.displayName} Battery`;
+    (this.service = this.accessory.getService(this.platform.Service.Valve) || this.accessory.addService(this.platform.Service.Valve)),
+    `${accessory.displayName}`;
 
     // To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
     // when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
@@ -79,75 +70,17 @@ export class Valve {
     // Do initial device parse
     this.parseStatus();
 
-    // Set Charging State
-    this.service.setCharacteristic(this.platform.Characteristic.ChargingState, 2);
+    // create handlers for required characteristics
+    this.service.getCharacteristic(this.platform.Characteristic.Active)
+      .onGet(this.handleActiveGet.bind(this))
+      .onSet(this.handleActiveSet.bind(this));
 
-    // Leak Sensor Service
-    if (this.device.leaksensor?.hide_leak) {
-      this.debugLog(`Leak Sensor: ${accessory.displayName} Removing Leak Sensor Service`);
-      this.leakService = this.accessory.getService(this.platform.Service.LeakSensor);
-      accessory.removeService(this.leakService!);
-    } else if (!this.leakService) {
-      this.debugLog(`Leak Sensor: ${accessory.displayName} Add Leak Sensor Service`);
-      (this.leakService = this.accessory.getService(this.platform.Service.LeakSensor) || this.accessory.addService(this.platform.Service.LeakSensor)),
-      `${accessory.displayName} Leak Sensor`;
+    this.service.getCharacteristic(this.platform.Characteristic.InUse)
+      .onGet(this.handleInUseGet.bind(this));
 
-      this.leakService.setCharacteristic(this.platform.Characteristic.Name, `${accessory.displayName} Leak Sensor`);
-    } else {
-      this.debugLog(`Leak Sensor: ${accessory.displayName} Leak Sensor Service Not Added`);
-    }
+    this.service.getCharacteristic(this.platform.Characteristic.ValveType)
+      .onGet(this.handleValveTypeGet.bind(this));
 
-    // Temperature Sensor Service
-    if (this.device.leaksensor?.hide_temperature) {
-      this.debugLog(`Leak Sensor: ${accessory.displayName} Removing Temperature Sensor Service`);
-      this.temperatureService = this.accessory.getService(this.platform.Service.TemperatureSensor);
-      accessory.removeService(this.temperatureService!);
-    } else if (!this.temperatureService) {
-      this.debugLog(`Leak Sensor: ${accessory.displayName} Add Temperature Sensor Service`);
-      (this.temperatureService =
-        this.accessory.getService(this.platform.Service.TemperatureSensor) || this.accessory.addService(this.platform.Service.TemperatureSensor)),
-      `${accessory.displayName} Temperature Sensor`;
-
-      this.temperatureService.setCharacteristic(this.platform.Characteristic.Name, `${accessory.displayName} Temperature Sensor`);
-
-      this.temperatureService
-        .getCharacteristic(this.platform.Characteristic.CurrentTemperature)
-        .setProps({
-          minValue: -273.15,
-          maxValue: 100,
-          minStep: 0.1,
-        })
-        .onGet(async () => {
-          return this.CurrentTemperature;
-        });
-    } else {
-      this.debugLog(`Leak Sensor: ${accessory.displayName} Temperature Sensor Service Not Added`);
-    }
-
-    // Humidity Sensor Service
-    if (this.device.leaksensor?.hide_humidity) {
-      this.debugLog(`Leak Sensor: ${accessory.displayName} Removing Humidity Sensor Service`);
-      this.humidityService = this.accessory.getService(this.platform.Service.HumiditySensor);
-      accessory.removeService(this.humidityService!);
-    } else if (!this.humidityService) {
-      this.debugLog(`Leak Sensor: ${accessory.displayName} Add Humidity Sensor Service`);
-      (this.humidityService =
-        this.accessory.getService(this.platform.Service.HumiditySensor) || this.accessory.addService(this.platform.Service.HumiditySensor)),
-      `${accessory.displayName} Humidity Sensor`;
-
-      this.humidityService.setCharacteristic(this.platform.Characteristic.Name, `${accessory.displayName} Humidity Sensor`);
-
-      this.humidityService
-        .getCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity)
-        .setProps({
-          minStep: 0.1,
-        })
-        .onGet(async () => {
-          return this.CurrentRelativeHumidity;
-        });
-    } else {
-      this.debugLog(`Leak Sensor: ${accessory.displayName} Humidity Sensor Service Not Added`);
-    }
 
     // Retrieve initial values and updateHomekit
     this.updateHomeKitCharacteristics();
@@ -165,35 +98,7 @@ export class Valve {
    */
   async parseStatus(): Promise<void> {
     // Leak Service
-    this.StatusActive = this.device.hasDeviceCheckedIn;
-    if (this.device.waterPresent === true) {
-      this.LeakDetected = 1;
-    } else {
-      this.LeakDetected = 0;
-    }
-    this.debugLog(`Leak Sensor: ${this.accessory.displayName} LeakDetected: ${this.LeakDetected}`);
-
-    // Temperature Service
-    if (!this.device.leaksensor?.hide_temperature) {
-      this.CurrentTemperature = this.device.currentSensorReadings.temperature;
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} CurrentTemperature: ${this.CurrentTemperature}°`);
-    }
-
-    // Humidity Service
-    if (!this.device.leaksensor?.hide_humidity) {
-      this.CurrentRelativeHumidity = this.device.currentSensorReadings.humidity;
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} CurrentRelativeHumidity: ${this.CurrentRelativeHumidity}%`);
-    }
-
-    // Battery Service
-    this.BatteryLevel = Number(this.device.batteryRemaining);
-    this.service.getCharacteristic(this.platform.Characteristic.BatteryLevel).updateValue(this.BatteryLevel);
-    if (this.device.batteryRemaining < 15) {
-      this.StatusLowBattery = this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
-    } else {
-      this.StatusLowBattery = this.platform.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
-    }
-    this.debugLog(`Leak Sensor: ${this.accessory.displayName} BatteryLevel: ${this.BatteryLevel},` + ` StatusLowBattery: ${this.StatusLowBattery}`);
+    this.Active = this.device.isAlive;
   }
 
   /**
@@ -209,7 +114,7 @@ export class Valve {
         })
       ).data;
       this.device = device;
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} device: ${superStringify(this.device)}`);
+      this.debugLog(`Valve: ${this.accessory.displayName} device: ${superStringify(this.device)}`);
       this.parseStatus();
       this.updateHomeKitCharacteristics();
     } catch (e: any) {
@@ -223,54 +128,60 @@ export class Valve {
    * Updates the status for each of the HomeKit Characteristics
    */
   async updateHomeKitCharacteristics(): Promise<void> {
-    if (this.BatteryLevel === undefined) {
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} BatteryLevel: ${this.BatteryLevel}`);
+    if (this.Active === undefined) {
+      this.debugLog(`Valve: ${this.accessory.displayName} Active: ${this.Active}`);
     } else {
-      this.service.updateCharacteristic(this.platform.Characteristic.BatteryLevel, this.BatteryLevel);
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} updateCharacteristic BatteryLevel: ${this.BatteryLevel}`);
-    }
-    if (this.StatusLowBattery === undefined) {
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} StatusLowBattery: ${this.StatusLowBattery}`);
-    } else {
-      this.service.updateCharacteristic(this.platform.Characteristic.StatusLowBattery, this.StatusLowBattery);
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} updateCharacteristic StatusLowBattery: ${this.StatusLowBattery}`);
-    }
-    if (!this.device.leaksensor?.hide_leak) {
-      if (this.LeakDetected === undefined) {
-        this.debugLog(`Leak Sensor: ${this.accessory.displayName} LeakDetected: ${this.LeakDetected}`);
-      } else {
-        this.leakService?.updateCharacteristic(this.platform.Characteristic.LeakDetected, this.LeakDetected);
-        this.debugLog(`Leak Sensor: ${this.accessory.displayName} updateCharacteristic LeakDetected: ${this.LeakDetected}`);
-      }
-      if (this.StatusActive === undefined) {
-        this.debugLog(`Leak Sensor: ${this.accessory.displayName} StatusActive: ${this.StatusActive}`);
-      } else {
-        this.leakService?.updateCharacteristic(this.platform.Characteristic.StatusActive, this.StatusActive);
-        this.debugLog(`Leak Sensor: ${this.accessory.displayName} updateCharacteristic StatusActive: ${this.StatusActive}`);
-      }
-    }
-    if (this.device.leaksensor?.hide_temperature && this.CurrentTemperature === undefined) {
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} CurrentTemperature: ${this.CurrentTemperature}`);
-    } else {
-      this.temperatureService?.updateCharacteristic(this.platform.Characteristic.CurrentTemperature, this.CurrentTemperature);
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} updateCharacteristic CurrentTemperature: ${this.CurrentTemperature}`);
-    }
-    if (this.device.leaksensor?.hide_humidity && this.CurrentRelativeHumidity === undefined) {
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} CurrentRelativeHumidity: ${this.CurrentRelativeHumidity}`);
-    } else {
-      this.humidityService?.updateCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity, this.CurrentRelativeHumidity);
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName}` + ` updateCharacteristic CurrentRelativeHumidity: ${this.CurrentRelativeHumidity}`);
+      this.service.updateCharacteristic(this.platform.Characteristic.Active, this.Active);
+      this.debugLog(`Valve: ${this.accessory.displayName} updateCharacteristic Active: ${this.Active}`);
     }
   }
 
+  /**
+   * Handle requests to get the current value of the "Active" characteristic
+   */
+  handleActiveGet() {
+    this.debugLog('Triggered GET Active');
+
+    // set this to a valid value for Active
+    const currentValue = this.platform.Characteristic.Active.INACTIVE;
+
+    return currentValue;
+  }
+
+  /**
+   * Handle requests to set the "Active" characteristic
+   */
+  handleActiveSet(value) {
+    this.debugLog('Triggered SET Active:', value);
+  }
+
+  /**
+   * Handle requests to get the current value of the "In Use" characteristic
+   */
+  handleInUseGet() {
+    this.debugLog('Triggered GET InUse');
+
+    // set this to a valid value for InUse
+    const currentValue = this.platform.Characteristic.InUse.NOT_IN_USE;
+
+    return currentValue;
+  }
+
+
+  /**
+   * Handle requests to get the current value of the "Valve Type" characteristic
+   */
+  handleValveTypeGet() {
+    this.debugLog('Triggered GET ValveType');
+
+    // set this to a valid value for ValveType
+    const currentValue = this.platform.Characteristic.ValveType.GENERIC_VALVE;
+
+    return currentValue;
+  }
+
   async apiError(e: any): Promise<void> {
-    this.service.updateCharacteristic(this.platform.Characteristic.BatteryLevel, e);
-    this.service.updateCharacteristic(this.platform.Characteristic.StatusLowBattery, e);
-    if (!this.device.leaksensor?.hide_leak) {
-      this.leakService?.updateCharacteristic(this.platform.Characteristic.LeakDetected, e);
-      this.leakService?.updateCharacteristic(this.platform.Characteristic.StatusActive, e);
-    }
-    //throw new this.platform.api.hap.HapStatusError(HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    this.service.updateCharacteristic(this.platform.Characteristic.Active, e);
   }
 
   async resideoAPIError(e: any): Promise<void> {
@@ -286,40 +197,40 @@ export class Valve {
       }
     }
     if (e.message.includes('400')) {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to ${this.action}, Bad Request`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to ${this.action}, Bad Request`);
       this.debugLog('The client has issued an invalid request. This is commonly used to specify validation errors in a request payload.');
     } else if (e.message.includes('401')) {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to ${this.action}, Unauthorized Request`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to ${this.action}, Unauthorized Request`);
       this.debugLog('Authorization for the API is required, but the request has not been authenticated.');
     } else if (e.message.includes('403')) {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to ${this.action}, Forbidden Request`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to ${this.action}, Forbidden Request`);
       this.debugLog('The request has been authenticated but does not have appropriate permissions, or a requested resource is not found.');
     } else if (e.message.includes('404')) {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to ${this.action}, Requst Not Found`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to ${this.action}, Requst Not Found`);
       this.debugLog('Specifies the requested path does not exist.');
     } else if (e.message.includes('406')) {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to ${this.action}, Request Not Acceptable`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to ${this.action}, Request Not Acceptable`);
       this.debugLog('The client has requested a MIME type via the Accept header for a value not supported by the server.');
     } else if (e.message.includes('415')) {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to ${this.action}, Unsupported Requst Header`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to ${this.action}, Unsupported Requst Header`);
       this.debugLog('The client has defined a contentType header that is not supported by the server.');
     } else if (e.message.includes('422')) {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to ${this.action}, Unprocessable Entity`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to ${this.action}, Unprocessable Entity`);
       this.debugLog(
         'The client has made a valid request, but the server cannot process it.' +
           ' This is often used for APIs for which certain limits have been exceeded.',
       );
     } else if (e.message.includes('429')) {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to ${this.action}, Too Many Requests`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to ${this.action}, Too Many Requests`);
       this.debugLog('The client has exceeded the number of requests allowed for a given time window.');
     } else if (e.message.includes('500')) {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to ${this.action}, Internal Server Error`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to ${this.action}, Internal Server Error`);
       this.debugLog('An unexpected error on the SmartThings servers has occurred. These errors should be rare.');
     } else {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to ${this.action},`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to ${this.action},`);
     }
     if (this.deviceLogging.includes('debug')) {
-      this.platform.log.error(`Leak Sensor: ${this.accessory.displayName} failed to pushChanges, Error Message: ${superStringify(e.message)}`);
+      this.platform.log.error(`Valve: ${this.accessory.displayName} failed to pushChanges, Error Message: ${superStringify(e.message)}`);
     }
   }
 
@@ -335,33 +246,33 @@ export class Valve {
       config['refreshRate'] = device.refreshRate;
     }
     if (Object.entries(config).length !== 0) {
-      this.infoLog(`Leak Sensor: ${this.accessory.displayName} Config: ${superStringify(config)}`);
+      this.infoLog(`Valve: ${this.accessory.displayName} Config: ${superStringify(config)}`);
     }
   }
 
   async refreshRate(device: settings.device & settings.devicesConfig): Promise<void> {
     if (device.refreshRate) {
       this.deviceRefreshRate = this.accessory.context.refreshRate = device.refreshRate;
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} Using Device Config refreshRate: ${this.deviceRefreshRate}`);
+      this.debugLog(`Valve: ${this.accessory.displayName} Using Device Config refreshRate: ${this.deviceRefreshRate}`);
     } else if (this.platform.config.options!.refreshRate) {
       this.deviceRefreshRate = this.accessory.context.refreshRate = this.platform.config.options!.refreshRate;
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} Using Platform Config refreshRate: ${this.deviceRefreshRate}`);
+      this.debugLog(`Valve: ${this.accessory.displayName} Using Platform Config refreshRate: ${this.deviceRefreshRate}`);
     }
   }
 
   async logs(device: settings.device & settings.devicesConfig): Promise<void> {
     if (this.platform.debugMode) {
       this.deviceLogging = this.accessory.context.logging = 'debugMode';
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} Using Debug Mode Logging: ${this.deviceLogging}`);
+      this.debugLog(`Valve: ${this.accessory.displayName} Using Debug Mode Logging: ${this.deviceLogging}`);
     } else if (device.logging) {
       this.deviceLogging = this.accessory.context.logging = device.logging;
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} Using Device Config Logging: ${this.deviceLogging}`);
+      this.debugLog(`Valve: ${this.accessory.displayName} Using Device Config Logging: ${this.deviceLogging}`);
     } else if (this.platform.config.options?.logging) {
       this.deviceLogging = this.accessory.context.logging = this.platform.config.options?.logging;
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} Using Platform Config Logging: ${this.deviceLogging}`);
+      this.debugLog(`Valve: ${this.accessory.displayName} Using Platform Config Logging: ${this.deviceLogging}`);
     } else {
       this.deviceLogging = this.accessory.context.logging = 'standard';
-      this.debugLog(`Leak Sensor: ${this.accessory.displayName} Logging Not Set, Using: ${this.deviceLogging}`);
+      this.debugLog(`Valve: ${this.accessory.displayName} Logging Not Set, Using: ${this.deviceLogging}`);
     }
   }
 
